2e80bbd4f637bea7b58a0633fc4cdb8791e8e233
Olenich <alexandrolenich06@gmail.com>
2025-03-24 09:55:26 +0200

    Добавьте файлы проекта.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть a: ");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть b: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Виберіть дію\n1. Додавання\n2. Віднімання\n3. Множення\n4. Ділення\n");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1: { Console.WriteLine($"{a} + {b} = {a + b} "); break; }
+                case 2: { Console.WriteLine($"{a} - {b} = {a - b}"); break; }
+                case 3: { Console.WriteLine($"{a} * {b} = {a * b}"); break; }
+                case 4: { Console.WriteLine($"{a} / {b} = {a / b}"); break; }
+                default: { Console.WriteLine("Неправильна дія"); break; }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
