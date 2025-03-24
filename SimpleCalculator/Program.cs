ab4249b51f4488eadf66510d0be5022eed4d4a45
Olenich <alexandrolenich06@gmail.com>
2025-03-24 10:04:30 +0200

    Доданий цикл while для можливості декілька разів використовувати програму без перезапуску

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,24 +10,26 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введіть a: ");
-            double a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введіть b: ");
-            double b = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Виберіть дію\n1. Додавання\n2. Віднімання\n3. Множення\n4. Ділення\n");
-
-            int choice = Convert.ToInt32(Console.ReadLine());
-            switch (choice)
+            while (true)
             {
-                case 1: { Console.WriteLine($"{a} + {b} = {a + b} "); break; }
-                case 2: { Console.WriteLine($"{a} - {b} = {a - b}"); break; }
-                case 3: { Console.WriteLine($"{a} * {b} = {a * b}"); break; }
-                case 4: { Console.WriteLine($"{a} / {b} = {a / b}"); break; }
-                default: { Console.WriteLine("Неправильна дія"); break; }
-            }
+                Console.WriteLine("Введіть a: ");
+                double a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введіть b: ");
+                double b = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadKey();
+                Console.WriteLine("Виберіть дію\n1. Додавання\n2. Віднімання\n3. Множення\n4. Ділення\n0. Вихід\n");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: { Console.WriteLine($"{a} + {b} = {a + b} "); break; }
+                    case 2: { Console.WriteLine($"{a} - {b} = {a - b}"); break; }
+                    case 3: { Console.WriteLine($"{a} * {b} = {a * b}"); break; }
+                    case 4: { Console.WriteLine($"{a} / {b} = {a / b}"); break; }
+                    case 0: return;
+                    default: { Console.WriteLine("Неправильна дія"); break; }
+                }
+            }
         }
     }
 }
